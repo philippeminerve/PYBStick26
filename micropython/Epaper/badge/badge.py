063b9e973daa8d95f1c17b1cf514f9f2cf87ff79
philippeminerve <35769720+philippeminerve@users.noreply.github.com>
2021-07-01 08:25:25 +0200

    Add files via upload

---
+++ micropython/Epaper/badge/badge.py
@@ -0,0 +1,19 @@
+import epaper2in9 # Bibliothèque ecran
+import TESTU # Image générée
+from machine import Pin, SPI # Bibliothèque de gestion des entrées sorties
+
+# Init PybStick26
+spi = SPI(baudrate=24000000,miso=Pin("S21"), mosi=Pin("S19", Pin.OUT), sck=Pin("S23", Pin.OUT))
+cs=Pin("S11");dc=Pin("S5");rst=Pin("S3");busy = Pin("S8")
+
+e = epaper2in9.EPD(spi, cs, dc, rst, busy)
+e.init()
+
+w = 128;h = 296;x = 0;y = 0
+
+#on vide l'écran
+e.clear_frame_memory(b'\xFF')
+image = ('\x3e'*1184).encode()
+e.set_frame_memory(image,x,y,w,h)
+
+e.display_frame()
